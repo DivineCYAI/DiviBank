83fd64d24be1791f1d9817baf243bff57415f185
DivineCYAI <divineomede017@gmail.com>
2026-07-31 03:00:02 +0100

    Update Divibank.py

--- Divibank.py
+++ Divibank.py
@@ -1,6 +1,3 @@
-#        6731628528  6732688974 Divi@673 1458
-# 6735978702  6739173608 Sanda@89     5455
-# 6734054548  6731137705 Dave@453 1989
 from rich.prompt import Prompt, IntPrompt
 from bank_helpers import (
     console, 
